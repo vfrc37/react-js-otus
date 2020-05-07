db758ad19cb097e281753578233f657812d8a097
vfrc37 <maxim.ermishkin@uchi.ru>
2020-05-07 23:42:39 +0300

    env(package.json, webpack.config.js): добавлен html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   entry: './src/index.js',
   output: {
@@ -9,4 +11,9 @@ module.exports = {
   devServer: {
     historyApiFallback: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ],
 };
