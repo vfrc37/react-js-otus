900f26afb4477bac7a3a1ec107d4433b205d7cf2
vfrc37 <maxim.ermishkin@uchi.ru>
2020-05-08 00:18:21 +0300

    env(package.json, webpack.config.js): добавлен babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,21 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'],
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)x?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      }
+    ]
   },
   devServer: {
     historyApiFallback: true,
