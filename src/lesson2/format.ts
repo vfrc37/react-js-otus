3e34929ad3b87f5170d01760bdc43dea8d6de1f6
vfrc37 <maxim.ermishkin@uchi.ru>
2020-05-17 00:26:30 +0300

    feat(parser.ts): обработка некорректных последовательностей символов

--- src/lesson2/format.ts
+++ src/lesson2/format.ts
@@ -1,7 +1,7 @@
 export const format = (str: string): string =>
   str
     .trim()
-    .replace(/(fib|cos|sin|tan)/g, " $1 ")
+    .replace(/(fib|sin|cos|tan)/g, " $1 ")
     .replace(/(\(|\)|!|\^)/g, " $1 ")
     .replace(/\*{2}\s*(\d+)/g, " ^ $1") // ** number === ^ number
     .replace(/\*{2}/g, " ^ 2") // alias to power of 2
--- src/lesson2/parser.ts
+++ src/lesson2/parser.ts
@@ -1,35 +1,67 @@
 import { isNumber } from "./helpers";
-import { mathOperators } from "./mathOperators";
 
 export type ParsedLineType = (number | string)[];
 
 export const parser = (line: string): ParsedLineType | null => {
   const stack = line.split(" ");
+  let openBracketsCounter = 0;
 
   return stack.reduce<ParsedLineType>((result, item, key) => {
-    const prevItem = stack[key - 1];
+    const nextItem = stack[key + 1];
+    const itemStr = String(item);
+    const nextItemStr = String(nextItem);
+    const isLastItem = key === stack.length - 1;
 
-    const isValidNumberPush = !isNumber(prevItem) && isNumber(item);
-    const isValidOperatorPush =
-      isNumber(prevItem) &&
-      !isNumber(item) &&
-      mathOperators.hasOwnProperty(item);
+    /**
+     * Possible items order:
+     * item   | nextItem
+     * number | sign, ), !
+     * )      | sign, ), !
+     * !      | sign, ), !
+     * sign   | number, (, func
+     * (      | number, (, func
+     * func   | (
+     */
 
-    const bracketsSymbols = ["(", ")"];
-
-    if (isValidNumberPush) {
-      result.push(Number(item));
-    } else if (isValidOperatorPush) {
-      result.push(item);
-    } else if (
-      bracketsSymbols.includes(item) ||
-      bracketsSymbols.includes(prevItem)
-    ) {
-      result.push(item);
-    } else {
-      result.push(item);
-      // throw new TypeError("Unexpected string");
+    switch (true) {
+      case /\!|\d+|\)/.test(itemStr):
+        if (!isLastItem && !/\+|\-|\*|\/|\!|\^|\)/.test(nextItemStr)) {
+          throw new TypeError(
+            `Invalid elements order: '${nextItemStr}' can't be after '${itemStr}'`
+          );
+        }
+        break;
+      case /\+|\-|\*|\/|\^|\(/.test(itemStr):
+        if (!isLastItem && !/\d+|\(|fib|sin|cos|tan/.test(nextItemStr)) {
+          throw new TypeError(
+            `Invalid elements order: '${nextItemStr}' can't be after '${itemStr}'`
+          );
+        }
+        break;
+      case /fib|sin|cos|tan/.test(itemStr):
+        if (!isLastItem && !/\(/.test(nextItemStr)) {
+          throw new TypeError(
+            `Invalid elements order: '${nextItemStr}' can't be after '${itemStr}'`
+          );
+        }
+        break;
+      default:
+        throw new TypeError(`Invalid element: '${itemStr}'`);
     }
+
+    if (itemStr === ")") openBracketsCounter -= 1;
+    if (itemStr === "(") openBracketsCounter += 1;
+
+    if (openBracketsCounter < 0) {
+      throw new TypeError("Invalid brackets: there are extra close brackets");
+    }
+
+    if (isLastItem === true && openBracketsCounter !== 0) {
+      throw new TypeError(
+        "Invalid brackets: number of close and open brackets is different"
+      );
+    }
+    result.push(isNumber(item) ? Number(item) : item);
     return result;
   }, []);
 };
